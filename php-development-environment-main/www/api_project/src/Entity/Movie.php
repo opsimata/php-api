ad465f40f1bb26316b559ea0982a38cc0467f253
Pedro <pedro.enrique1599@icloud.com>
2023-09-27 00:16:24 -0300

    starting commit

---
+++ php-development-environment-main/www/api_project/src/Entity/Movie.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 2000)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ php-development-environment-main/www/backend.php
@@ -0,0 +1,13 @@
+<?php
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+echo "O usuário {$username} está tentando logar com a senha {$password}";
+
+$nota1 = 4;
+$nota2 = 8;
+
+$media = ($nota1 + $nota2)/2;
+
+echo "média das notas: {$media}";
